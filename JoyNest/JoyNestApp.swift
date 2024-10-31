e9dd4e06efcd6c27ab09c39709309ac7dbca424d
GuyaL253 <guyal253@gmail.com>
2024-10-31 20:01:03 +0200

    Initial Commit

---
+++ JoyNest/JoyNestApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoyNestApp.swift
+//  JoyNest
+//
+//  Created by Guy Almog on 31/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct JoyNestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JoyNestTests/JoyNestTests.swift
@@ -0,0 +1,17 @@
+//
+//  JoyNestTests.swift
+//  JoyNestTests
+//
+//  Created by Guy Almog on 31/10/2024.
+//
+
+import Testing
+@testable import JoyNest
+
+struct JoyNestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
